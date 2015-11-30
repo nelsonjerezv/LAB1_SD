987704a973917d08fa3dafc060c7666026a40600
Nelson <Nelson@Nelson-Notebook>
2015-11-30 16:48:03 -0400

    REST en consultas cliente, lectura REST en Servidor

--- src/tcpserver/MultiThreadServer.java
+++ src/tcpserver/MultiThreadServer.java
@@ -45,17 +45,69 @@ public class MultiThreadServer implements Runnable {
            
            //Recibimos el dato del cliente y lo mostramos en el server
            fromClient =inFromClient.readLine();
-           System.out.println("Received: " + fromClient);
-           
-           //Se procesa el dato recibido
-           //processedData = fromClient.toUpperCase() + '\n';
-           String reverse = new StringBuffer(fromClient).reverse().toString() + '\n';
-           
-           
-           //Se le envia al cliente
-           outToClient.writeBytes(reverse);
-           
-           //Thread.interrupted();
+                                    // System.out.println("Received: " + fromClient);
+
+                                     //Se procesa el dato recibido
+                                     //processedData = fromClient.toUpperCase() + '\n';
+                                     //String reverse = new StringBuffer(fromClient).reverse().toString() + '\n';
+
+                                     //Se le envia al cliente
+                                     //outToClient.writeBytes(reverse);
+           System.out.println("===== ===== ===== ===== =====");
+
+            //String request = requests[i];
+
+            //String[] tokens = request.split(" ");
+            String[] tokens = fromClient.split(" ");
+            String parametros = tokens[1];
+
+            String http_method = tokens[0];
+
+            String[] tokens_parametros = parametros.split("/");
+
+            String resource = tokens_parametros.length > 1 ? tokens_parametros[1] : "";
+            String id = tokens_parametros.length > 2 ? tokens_parametros[2] : "";
+
+            String meta_data = tokens.length > 2 ? tokens[2] : "";
+
+            System.out.println("Consulta: " + fromClient);
+            System.out.println("HTTP METHOD: " + http_method);
+            System.out.println("Resource: " + resource);
+            System.out.println("ID:          " + id);
+            System.out.println("META DATA:    " + meta_data);
+            switch (http_method) {
+                case "GET":
+                    if (id == "") {
+                        System.out.println("Buscando en la base de datos los ultimos 10 registros de tipo '" + resource + "'");
+                        // buscar en el cache
+                        // hit o miss
+                    } else {
+                        System.out.println("Buscando en el cache de '" + resource + "' el registro con id " + id);
+                    }
+                    break;
+                case "POST":
+                    System.out.println("Creando un usuario con los siguientes datos: (" + meta_data + ")");
+                    for (String params : meta_data.split("&")) {
+                        String[] parametros_meta = params.split("=");
+                        System.out.println("\t* " + parametros_meta[0] + " -> " + parametros_meta[1]);
+                    }
+                    break;
+                case "PUT":
+                    System.out.println("Actualizando el usuario con id " + id + " con los siguientes datos (" + meta_data + ")");
+                    for (String params : meta_data.split("&")) {
+                        String[] parametros_meta = params.split("=");
+                        System.out.println("\t* " + parametros_meta[0] + " -> " + parametros_meta[1]);
+                    }
+                    break;
+                case "DELETE":
+                    System.out.println("Borrando el recurso de tipo '" + resource + "' con id " + id);
+                    break;
+                default:
+                    System.out.println("Not a valid HTTP Request");
+                    break;
+            }
+            //Se le envia al cliente
+            outToClient.writeBytes("Consulta Procesada\n");
            
        } catch (IOException ex) {
            Logger.getLogger(MultiThreadServer.class.getName()).log(Level.SEVERE, null, ex);
--- src/tcpserver/TCPClient.java
+++ src/tcpserver/TCPClient.java
@@ -19,31 +19,46 @@ public class TCPClient {
         String sentence;
         String fromServer;
         int a =0;
-        while(a<5){
-            //Buffer para recibir desde el usuario
-            BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-
-            //Socket para el cliente (host, puerto)
-            Socket clientSocket = new Socket("localhost", 1234);
-
-            //Buffer para enviar el dato al server
-            DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-
-            //Buffer para recibir dato del servidor
-            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-            //Leemos del cliente y lo mandamos al servidor
-            sentence = inFromUser.readLine();
-            outToServer.writeBytes(sentence + '\n');
-
-            //Recibimos del servidor
-            fromServer = inFromServer.readLine();
-            System.out.println("Server response: " + fromServer);
-
-            //Cerramos el socket
-            clientSocket.close();
-            a++;
+        
+        String[] requests = {
+            "GET /respuestas/hola", // <p>hola mundo</>
+            "GET /users",
+            "GET /users/1234",
+            "GET /users/55556",
+            "ABC /users/1234",
+            "POST /users username=gbenussi&password=contrasena", // adasdas
+            "POST /respuestas/hola body=<p>hola mundo</>",
+            "POST /respuestas/hola body=<asdasdasdasdasd",
+            "PUT /respuestas/hola title=hola+mundo", // aasdsadasdsa
+            "PUT /users/1234 username=giovanni", // aasdsadasdsa
+            "DELETE /users/1234",};
+        
+        for (String request : requests) {
+            
+                //Buffer para recibir desde el usuario
+                //BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+                
+                //Socket para el cliente (host, puerto)
+                Socket clientSocket = new Socket("localhost", 1234);
+                
+                //Buffer para enviar el dato al server
+                DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+                
+                //Buffer para recibir dato del servidor
+                BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                
+                //Leemos del cliente y lo mandamos al servidor
+                //sentence = inFromUser.readLine();
+                outToServer.writeBytes(request + '\n');
+                
+                //Recibimos del servidor
+                fromServer = inFromServer.readLine();
+                System.out.println("Server response: " + fromServer);
+                
+                //Cerramos el socket
+                clientSocket.close();
+                a++;
+            }
         }
-    }
     
 }
